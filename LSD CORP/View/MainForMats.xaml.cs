827d610ec66d83eaeef7bba36294aec28b1a3c7a
skazo4nik262 <skazo4nik@DESKTOP-81KEU8S>
2025-06-24 16:33:31 +1000

    Добавьте файлы проекта.

---
+++ LSD CORP/View/MainForMats.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LSD_CORP.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MainForMats.xaml
+    /// </summary>
+    public partial class MainForMats : Window
+    {
+        public MainForMats()
+        {
+            InitializeComponent();
+        }
+
+        private void AddClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DelClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
